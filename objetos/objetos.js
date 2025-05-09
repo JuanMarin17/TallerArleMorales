8fe9d3f87990d87d4e20ae2e0322c4837bddf963
JuanMarin <juandavidmr77@gmail.com>
2025-05-09 09:33:22 -0500

    'objetos'

---
+++ objetos/objetos.js
@@ -0,0 +1,20 @@
+const clientes = [
+  {
+    nombre: "Juan",
+    edad: 25,
+  },
+  {
+    nombre: "Hugo",
+    edad: 43,
+  },
+  {
+    nombre: "Shirley",
+    edad: 17,
+  },
+  {
+    nombre: "David",
+    edad: 16,
+  },
+];
+
+console.log(clientes[0].nombre)
